c243d989f870326ed479d3e476621af19ba8f7c2
briskgreen <briskgreen@163.com>
2013-04-26 00:20:51 +0800

    updata

--- pre_ui.c
+++ pre_ui.c
@@ -13,4 +13,14 @@ int main(int argc,char **argv)
 	GtkWidget *label;
 	GtkWidget *entry;
 	CONFDATA conf;
+
+	gtk_init(&argc,&argv);
+
+	dialog=gtk_dialog_new();
+	gtk_window_set_title(GTK_WINDOW(dialog),_("Preferences"));
+
+	notebook=gtk_notebook_new();
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox),notebook,
+			FALSE,FALSE,0);
+	gtk_widget_show(notebook);
 }
