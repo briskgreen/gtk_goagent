1cb5e0d8ed95576b0975e47646fee9098489b058
briskgreen <briskgreen@163.com>
2013-05-12 23:55:54 +0800

    updata

--- autoupgrade.c
+++ autoupgrade.c
@@ -2,6 +2,15 @@
 
 void error_quit(const char *msg);
 char *get_version(char *path);
+size_t is_upgrade_goagent(char *ptr,size_t size,size_t nmebm,
+		void *stream);
+size_t is_upgrade_gtk_goagent(char *ptr,size_t size,size_t nmebm,
+		void *stream);
+
+void download_file(char *path,char *is_upload);
+
+char *goagent_version;
+char *gtk_goagent_version;
 
 int main(int argc,char **argv)
 {
@@ -29,20 +38,17 @@ int main(int argc,char **argv)
 		fclose(fp);
 	}*/
 
+	goagent_version=get_version(conf.proxy_py_path);
+	gtk_goagent_version=get_version("readme");
+
 	if(strcmp(argv[1]"1")==0)
-		auto_upgrade_goagent(GOAGENT_URL,conf.goagent_path,
-				get_version(conf.proxy_py_path));
+		auto_upgrade_goagent(GOAGENT_URL);
 	else if(strcmp(argv[1],"2")==0)
-		auto_upgrade_gtk_goagent(GTK_GOAGENT_URL,
-				conf.gtk_goagent_path,get_version(
-					"readme"));
+		auto_upgrade_gtk_goagent(GTK_GOAGENT_URL);
 	else if(strcmp(argv[1],"3")==0)
 	{
-		auto_upgrade_goagent(GOAGENT_URL,conf.goagent_path,
-				get_version(conf.proxy_py_path));
-		auto_upgrade_gtk_goagent(GTK_GOAGENT_URL,
-				conf.gtk_goagent_path,get_version(
-					"readme"));
+		auto_upgrade_goagent(GOAGENT_URL);
+		auto_upgrade_gtk_goagent(GTK_GOAGENT_URL);
 	}
 
 	return 0;
@@ -100,3 +106,127 @@ char *get_version(char *path)
 	return version;
 }
 
+void auto_upgrade_goagent(char *url)
+{
+	if(fork()==0)
+	{
+		CURL *curl;
+
+		curl=curl_easy_init();
+		curl_easy_setopt(curl,CURLOPT_URL,url);
+		curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,
+				is_upgrade_goagent);
+		curl_easy_setopt(curl,CURLOPT_PROXY,PROXY);
+
+		while(1)
+		{
+			curl_easy_perform(curl);
+			sleep(10*60);
+		}
+	}
+}
+
+void auto_upgrade_gtk_goagent(char *url)
+{
+	if(fork()==0)
+	{
+		CURL *curl;
+
+		curl=curl_easy_init();
+		curl_easy_setopt(curl,CURLOPT_URL,url);
+		curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,
+				is_upgrade_gtk_goagent);
+		curl_easy_setopt(curl,CURLOPT_PROXY,PROXY);
+
+		while(1)
+		{
+			curl_easy_perform(curl);
+			sleep(10*60);
+		}
+	}
+}
+
+size_t is_upgrade_goagent(char *ptr,size_t size,size_t nmebm,
+		void *stream)
+{
+	char buf[40];
+	char path[100];
+	int i,j;
+
+	if(strstr(ptr,"https://nodeload.github.com"))
+	{
+		bzero(buf,sizeof(buf));
+
+		for(i=0;ptr[i];++i)
+		{
+			if(ptr[i] == '"')
+			{
+				bzero(temp,sizeof(temp));
+
+				snprintf(temp,39,"%s",ptr+i);
+
+				if(strstr(temp,"https://nodeload.github.com"))
+					goto ok;
+			}
+		}
+ok:
+		if(ptr[i] == '\0')
+			return 0;
+
+		for(++i,j=0;ptr[i] != '"';++i,++j)
+			path[j]=ptr[i];
+	}
+
+	if(strstr(ptr,"最近更新") && strstr(ptr,"</li><li><tt>"))
+	{
+		bzero(buf,sizeof(buf));
+
+		for(i=0;ptr[i] != '[';++i);
+		for(j=0;ptr[i-1] != ']';++i,++j)
+			buf[j]=ptr[i];
+
+		buf[j]=' ';
+
+		for(++j;ptr[i] != ' ';++i);
+		for(++i;ptr[i] != ' ';++i,++j)
+			buf[j]=ptr[i];
+
+		if(!strstr(buf,goagent_version))
+		{
+			gtk_init(NULL,NULL);
+			if(message_box_ok(_("Have New Version GoAgent Do You Want To Upgrade Now?")))
+				download_file(path,buf);
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
+
+	return nmebm;
+}
+
+void download_file(char *path,char *is_upload)
+{
+	CURL *curl;
+	FILE *fp;
+
+	gtk_init(NULL,NULL);
+
+	if((fp=fopen("/tmp/$goagent&","w"))==NULL)
+	{
+		message_box(NULL,strerror(errno));
+		return;
+	}
+
+	curl=curl_easy_init();
+	curl_easy_setopt(curl,CURLOPT_URL,path);
+	curl_easy_setopt(curl,CURLOPT_WRITEDATA,fp);
+	curl_easy_perform(curl);
+
+	fclose(fp);
+	curl_easy_cleanup(curl);
+
+	if(strstr(is_upload,"是"))
+		message_box(NULL,_("This Version Should Upload Again"));
+}
--- autoupgrade.h
+++ autoupgrade.h
@@ -7,10 +7,11 @@
 
 #define GOAGENT_URL "https://code.google.com/p/goagent/"
 #define GTK_GOAGENT_URL "https://briskgreen.github.io/Download/Gtk GoAgent"
+#define PROXY "127.0.0.1:8087"
 
-void auto_upgrade_goagent(char *url,char *path,char *version);
+void auto_upgrade_goagent(char *url);
 
-void auto_upgrade_gtk_goagent(char *url,char *path,char *version);
+void auto_upgrade_gtk_goagent(char *url);
 
 void unzip(char *path);
 
